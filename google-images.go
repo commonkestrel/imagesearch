4b79d5a08c6448b67928643c29ed18afb624e8f4
Jibble330 <jettb6@gmail.com>
2022-10-01 15:54:11 -0700

    Add documentation

--- google-images.go
+++ google-images.go
@@ -1,4 +1,10 @@
-package gis
+//A package designed to search Google Images based on the input query and arguments. These images may be protected under copyright, and you shouldn't do anything punishable with them, like using them for commercial use. This library is inspired by the Python library [google-images-download] created by [hardikvasa] and maintained by [joeclinton1], but ported to Go and with some quality of life improvements, such as being able to retrieve urls as well. Essentially, this package is a port of the Python library [GoogleImageScraper] to Go.
+//
+//[google-images-download]: https://www.github.com/joeclinton1/google-images-download
+//[hardikvasa]: https://www.github.com/hardikvasa
+//[joeclinton1]: https://www.github.com/joeclinton1
+//[GoogleImageScraper]: https://www.github.com/Jibble330/GoogleImageScraper
+package imagesearch
 
 import (
     "os"
@@ -13,13 +19,22 @@ import (
     "path/filepath"
 )
 
+//Contains information about an image including the url of the image, the url of the source, and the website it came from. Example: 
+//  Image {
+//      Url: www.example.com/static/image.png
+//      Source: www.example.com/article
+//      Base: example.com
+//  }
+//
 type Image struct {
     Url       string `json:"url"`
     Source    string `json:"source"`
     Base      string `json:"base"`
 }
 
-var ( //Define arguments
+//These variables are all of the possible arguments that can be passed into Images, Download, and Urls. These are used by passing gis.{Argument}.{Option} into the arguments parameter. For Example: 
+//  urls, err := gis.Urls("example", 0, gis.Color.Red, gis.License.CreativeCommons)
+var (
     Color = struct {
         Red, Orange, Yellow, Green, Teal, Blue, Purple, Pink, White, Gray, Black, Brown string
     }{Red: "isc:red", Orange: "isc:orange", Yellow: "isc:yellow", Green: "isc:green", Teal: "isc:teel", Blue: "isc:blue", Purple: "isc:purple", Pink: "isc:pink", White: "isc:white", Gray: "isc:gray", Black: "isc:black", Brown: "isc:brown"}
@@ -49,6 +64,9 @@ var ( //Define arguments
     }{Jpg: "ift:jpg", Gif: "ift:gif", Png: "ift:png", Bmp: "ift:bmp", Svg: "ift:svg", Webp: "webp", Ico: "ift:ico", Raw: "ift:craw"}
 )
 
+
+//Searches for the query along with the given arguments, and returns a slice of Image objects.
+//The amount of images does not exceed the limit unless the limit is 0, in which case it will return all images found.
 func Images(query string, limit int, arguments ...string) ([]Image, error) {
     url := buildUrl(query, arguments)
 
@@ -62,13 +80,16 @@ func Images(query string, limit int, arguments ...string) ([]Image, error) {
         return []Image{}, err
     }
 
-    if len(images) > limit {
+    if len(images) > limit && limit > 0 {
         images = images[:limit]
     }
 
     return images, nil
 }
 
+
+//Searches for the query along with the given arguments, and returns a slice of the image urls.
+//The amount of images does not exceed the limit unless the limit is 0, in which case it will return all urls found.
 func Urls(query string, limit int, arguments ...string) ([]string, error) {
     url := buildUrl(query, arguments)
 
@@ -82,7 +103,7 @@ func Urls(query string, limit int, arguments ...string) ([]string, error) {
         return []string{}, err
     }
 
-    if len(images) > limit {
+    if len(images) > limit && limit > 0 {
         images = images[:limit]
     }
 
@@ -94,9 +115,12 @@ func Urls(query string, limit int, arguments ...string) ([]string, error) {
     return urls, nil
 }
 
-func Download(query string, limit int, dir string) ([]string, int, error) {
+//Searches for the given query along with the given argumetnts and downloads the images into the given directory.
+//The amount of images does not exceed the limit unless the limit is 0, in which case it will download all images found.
+//Returns a slice of the absolute paths of all downloaded images.
+func Download(query string, limit int, dir string, arguments ...string) ([]string, int, error) {
     dir = strings.ReplaceAll(dir, "\\", "/")
-    urls, err := Urls(query, limit)
+    urls, err := Urls(query, limit, arguments...)
     if err != nil {
         return []string{}, 0, nil
     }
@@ -114,7 +138,7 @@ func Download(query string, limit int, dir string) ([]string, int, error) {
             matches, _ = filepath.Glob(pat)
         }
 
-        file, err := downloadImage(url, dir, query+strconv.Itoa(suffix))
+        file, err := DownloadImage(url, dir, query+strconv.Itoa(suffix))
         if err != nil {
             errs++
         }
@@ -125,7 +149,21 @@ func Download(query string, limit int, dir string) ([]string, int, error) {
     return paths, errs, nil
 }
 
-func downloadImage(url, dir, name string) (string, error) {
+//Given the url of the image, the directory to download to, and the name of the file *without extension*, this will find the type of image and download it to the given directory.
+//Warning: This will overwrite any image file with the same name, if the extension matches, so make sure to keep the name unique.
+//You can check if a file with the name already exists with the following code: 
+//
+//  import (
+//      "path"
+//      "path/filepath"
+//  )
+//  
+//  func exists(dir, name string) bool {
+//      pat := path.Join(dir, name) + ".*"
+//      matches, _ := filepath.Glob(pat)
+//      return len(matches) > 0
+//  }
+func DownloadImage(url, dir, name string) (string, error) {
     client := http.DefaultClient
     req, _ := http.NewRequest("GET", url, nil)
     req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36")
@@ -162,7 +200,6 @@ func downloadImage(url, dir, name string) (string, error) {
     return f.Name(), nil
 }
 
-
 func buildUrl(query string, arguments []string) string {
     url := "https://www.google.com/search?tbm=isch&q=" + query
 
@@ -196,7 +233,6 @@ func findImages(page string) ([]Image, error) {
     imageObjects := imageJson[56].([]interface{})[1].([]interface{})[0].([]interface{})[0].([]interface{})[1].([]interface{})[0].([]interface{})
 
     var images []Image
-
     for _, imageObject := range imageObjects {
         obj := imageObject.([]interface{})[0].([]interface{})[0].(map[string]interface{})["444383007"].([]interface{})[1]
         if obj != nil {
